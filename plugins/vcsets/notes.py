54f17885af0113d35aa517364c05ac08d3eacd64
Dash Eclipse <dashezup@disroot.org>
2021-04-20 17:11:10 +0000

    plugins.vcsets.notes: dirty fix for command /notes@ezupdevbot since Telegram Android client automatically append bot username to commands when the one press the blue text command

--- plugins/vcsets/notes.py
+++ plugins/vcsets/notes.py
@@ -38,19 +38,20 @@ with open(json_file) as f:
     & filters.text
     & ~filters.edited
     & (filters.command(list(notes.keys()), prefixes="#")
-       | filters.command("notes", prefixes="/"))
+       | filters.command(["notes", "notes@ezupdevbot"], prefixes="/"))
 )
 async def show_notes(_, m: Message):
     if len(m.command) != 1:
         return
+    section = m.command[0].split("@")[0]
     m_target, quote = (m.reply_to_message or m), bool(m.reply_to_message)
     m_response = await m_target.reply_text(
-        notes[m.command[0]],
+        notes[section],
         quote=quote,
         disable_web_page_preview=True
     )
     await m.delete()
-    key = 'list' if m.command[0] == 'notes' else 'note'
+    key = 'list' if section == 'notes' else 'note'
     if response.get(key) is not None:
         await response[key].delete()
     response[key] = m_response
