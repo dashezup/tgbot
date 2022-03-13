f4ec2fb50fec2f2afe53c69505e9b01acda4ff4c
Dash Eclipse <dashezup@disroot.org>
2022-03-13 19:35:44 +0000

    feat: tools.apple_health: add caption

--- plugins/tools/apple_health.py
+++ plugins/tools/apple_health.py
@@ -4,15 +4,14 @@ from io import StringIO, BytesIO
 import xml.etree.ElementTree as ET
 from zipfile import ZipFile
 
-from pyrogram import Client, filters
+from pyrogram import Client, filters, emoji
 from pyrogram.types import Message
 
 MAX_ZIP_FILE_SIZE = 10485760
 
 
 @Client.on_message(filters.document
-                   & filters.incoming
-                   & filters.edited)
+                   & filters.incoming)
 async def command_apple_health(_, m: Message):
     doc = m.document
     valid_file = (
@@ -37,10 +36,26 @@ async def command_apple_health(_, m: Message):
         for x in xml_root.findall('Record')
         if x.get('type') == "HKQuantityTypeIdentifierBodyMass"
     ]
+    if not records:
+        return
     sio = StringIO()
     writer = csv.writer(sio, quoting=csv.QUOTE_NONNUMERIC, lineterminator='\n')
     writer.writerows(records)
     bio = BytesIO(sio.getvalue().encode("utf-8"))
     bio.name = "weight.csv"
+    caption = (
+        "{} **Apple Health - Body Measurements - Weight** ({} records)\n"
+        + "`< {} ({} {})`\n"
+        + "`> {} ({} {})`"
+    ).format(
+        emoji.MOBILE_PHONE_WITH_ARROW,
+        len(records),
+        records[0][0][:10],
+        records[0][1],
+        records[0][2],
+        records[-1][0][:10],
+        records[-1][1],
+        records[-1][2],
+    )
     await m.reply_chat_action("upload_document")
-    await m.reply_document(bio, quote=True)
+    await m.reply_document(bio, caption=caption, quote=True)
